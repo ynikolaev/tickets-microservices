0497d14bcf8a0868d4264e8a5184d31fdcbb4047
Yan Nikolaev <yannikolaev@gmail.com>
2022-04-12 21:56:29 +0100

    amended log

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Starting service...');
+  console.log('Starting Auth service...');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY env variable is not defined');
